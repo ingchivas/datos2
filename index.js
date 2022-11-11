3517b986f9e846b2504336fb50b18b49c54c6655
Your Name <you@example.com>
2022-11-10 18:42:43 -0600

    a

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { Client } = require('pg');
 const { faker } = require('@faker-js/faker');
+const express = require('express');
+const app = express();
+
+
 const conndata = {
     user: 'postgres',
     host: 'localhost',
@@ -34,18 +38,53 @@ client.query(createUsersTable, (err, res) => {
 // Query to create random users and insert into the users table
 const insertUsers = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`;
 
-for (let i = 0; i < 100; i++) {
-    const name = faker.name.findName();
-    const email = faker.internet.email();
-    const password = faker.internet.password();
+// for (let i = 0; i < 100; i++) {
+//     const name = faker.name.findName();
+//     const email = faker.internet.email();
+//     const password = faker.internet.password();
 
-    client.query(insertUsers, [name, email, password], (err, res) => {
+//     client.query(insertUsers, [name, email, password], (err, res) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('User added');
+//         }
+//     });
+// }
+
+app.get('/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, result) => {
         if (err) {
             console.log(err);
         } else {
-            console.log('User added');
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    users: result.rows,
+                },
+            });
         }
     });
-}
+});
 
-client.end();
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    client.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    users: result.rows,
+                },
+            });
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    console.log('http://localhost:3000');
+});
